817d5b7b9e068b107bf444e11dd13f1f01f39bcd
ma2012924@gmail.com <ma2012924@gmail.com>
2025-08-02 19:40:33 +0300

    تحديث الإعدادات وملفات النشر

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kexxee$)nv!e)ozge%$-+kj-bqv+1@ot1^oy#79&#hcwpv622c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['ahmedmohamed65756.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,8 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'staticfiles')]
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
